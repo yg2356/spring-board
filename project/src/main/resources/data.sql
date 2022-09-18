418caa5c6c4241d35fe4b7baf896c3b82c6fa523
yoongu <kmnfcg@naver.com>
2022-09-18 17:41:39 +0900

    게시판 수정

--- project/src/main/resources/data.sql
+++ project/src/main/resources/data.sql
@@ -1,22 +1,22 @@
-INSERT INTO article(id, title, content) VALUES (1, '가가가가', '1111');
-INSERT INTO article(id, title, content) VALUES (2, '가가가나', '2222');
-INSERT INTO article(id, title, content) VALUES (3, '가가가다', '3333');
+INSERT INTO article(id, title, content) VALUES (1, '안녕하세요', '반갑습니다');
+INSERT INTO article(id, title, content) VALUES (2, '안녕히가세요', '즐거웠습니다');
+INSERT INTO article(id, title, content) VALUES (3, '또만나요', '잘가고');
 
 --article 더미 데이터
-INSERT INTO article(id, title, content) VALUES (4, '당신의 인생 영화는?', '댓글');
-INSERT INTO article(id, title, content) VALUES (5, '당신의 소울 프드는?', '댓글ㄱ');
-INSERT INTO article(id, title, content) VALUES (6, '당신의 취미는?', '댓글ㄱㄱ');
+INSERT INTO article(id, title, content) VALUES (4, '인생게임?', '댓글');
+INSERT INTO article(id, title, content) VALUES (5, '인생노래?', '댓글');
+INSERT INTO article(id, title, content) VALUES (6, '취미는?', '댓글');
 
 -- comment 더미 데이터
 ---- 4번 게시글의 댓글들
-INSERT INTO comment(id, article_id, nickname, body) VALUES(1, 4, 'Park', '굳 윌 헌팅');
-INSERT INTO comment(id, article_id, nickname, body) VALUES(2, 4, 'Kim', '아이 엠 샘');
-INSERT INTO comment(id, article_id, nickname, body) VALUES(3, 4, 'Choi', '쇼생크의 탈출');
+INSERT INTO comment(id, article_id, nickname, body) VALUES(1, 4, '박', '카트');
+INSERT INTO comment(id, article_id, nickname, body) VALUES(2, 4, '김', '로아');
+INSERT INTO comment(id, article_id, nickname, body) VALUES(3, 4, '최', '디아');
 ---- 5번 게시글의 댓글들
-INSERT INTO comment(id, article_id, nickname, body) VALUES(4, 5, 'Park', '치킨');
-INSERT INTO comment(id, article_id, nickname, body) VALUES(5, 5, 'Kim', '샤브샤브');
-INSERT INTO comment(id, article_id, nickname, body) VALUES(6, 5, 'Choi', '초밥');
+INSERT INTO comment(id, article_id, nickname, body) VALUES(4, 5, '이', '모놀로그');
+INSERT INTO comment(id, article_id, nickname, body) VALUES(5, 5, '오', '다이너마이트');
+INSERT INTO comment(id, article_id, nickname, body) VALUES(6, 5, '구', '나무');
 ---- 6번 게시글의 댓글들
-INSERT INTO comment(id, article_id, nickname, body) VALUES(7, 6, 'Park', '조깅');
-INSERT INTO comment(id, article_id, nickname, body) VALUES(8, 6, 'Kim', '유튜브');
-INSERT INTO comment(id, article_id, nickname, body) VALUES(9, 6, 'Choi', '독서');
+INSERT INTO comment(id, article_id, nickname, body) VALUES(7, 6, '류', '야구');
+INSERT INTO comment(id, article_id, nickname, body) VALUES(8, 6, '박', '유튜브');
+INSERT INTO comment(id, article_id, nickname, body) VALUES(9, 6, '송', '독서');
